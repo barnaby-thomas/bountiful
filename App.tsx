4dc75e4abefbde4deefec888a0d9d67a810b49c1
Barnaby Thomas <barnabythomas@Barnabys-MacBook-Pro.local>
2026-05-14 14:04:04 +0100

    create fonts.ts file to store fonts

--- App.tsx
+++ App.tsx
@@ -2,29 +2,42 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import { fonts } from './const/fonts';
+import { colours } from './const/colours';
 
 const Tab = createBottomTabNavigator();
 
 function MapScreen() {
-  return <View style={{flex:1,alignItems:'center',justifyContent:'center'}}><Text>Map</Text></View>;
+  return <View style={{flex:1,alignItems:'center',justifyContent:'center', backgroundColor: colours.background}}>
+            <Text>Map</Text>
+          </View>;
 }
 
 function ScanScreen() {
-  return <View style={{flex:1,alignItems:'center',justifyContent:'center'}}><Text>Scan</Text></View>;
+  return <View style={{flex:1,alignItems:'center',justifyContent:'center', backgroundColor: colours.background}}>
+            <Text>Scan</Text>
+         </View>;
 }
 
-function EncyclopaediaScreen() {
-  return <View style={{flex:1,alignItems:'center',justifyContent:'center'}}><Text>My Plants</Text></View>;
+function EncyclopediaScreen() {
+  return (
+    <View style={{flex:1, alignItems:'center', justifyContent:'center', backgroundColor: colours.background}}>
+      <Text style={{fontFamily: fonts.heading, fontSize:28}}>My Plants</Text>
+      <Text style={{fontFamily: fonts.body, fontSize:16}}>Your collection</Text>
+    </View>
+  );
 }
 
 export default function App() {
+
   return (
     <NavigationContainer>
       <StatusBar style="auto" />
       <Tab.Navigator>
         <Tab.Screen name="Map" component={MapScreen} />
         <Tab.Screen name="Scan" component={ScanScreen} />
-        <Tab.Screen name="Plants" component={EncyclopaediaScreen} />
+        <Tab.Screen name="Plants" component={EncyclopediaScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
---
+++ const/fonts.ts
@@ -0,0 +1,6 @@
+export const fonts = {
+  heading: 'Platypi_700Bold',
+  headingRegular: 'Platypi_400Regular',
+  body: 'DMSans_400Regular',
+  bodyMedium: 'DMSans_500Medium',
+}
