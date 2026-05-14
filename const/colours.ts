5f9c65b7f7f706a64d663db2f4cd40f2c0ad247c
Barnaby Thomas <barnabythomas@Barnabys-MacBook-Pro.local>
2026-05-14 13:29:10 +0100

    create seperate file with app colour variables

---
+++ const/colours.ts
@@ -0,0 +1,12 @@
+export const colours = {
+    background: '#E8F5E8',
+    darkGreenFill: '#3D6B3D',
+    searchBarBackground: '#C8DFC8',
+    fadedBlackText: '#000000BF',
+    black: '#000000',
+    white: '#FFFFFF',
+    greenText: '#3D6B3D',
+    privatePin: '#80B9FF',
+    publicPin: '#FFBB80',
+    userPin: '#5A8F5A',
+}
