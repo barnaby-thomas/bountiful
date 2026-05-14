74a9e80950c0fec646bb1ebeac7ea985b2028378
Barnaby Thomas <barnabythomas@Barnabys-MacBook-Pro.local>
2026-05-14 12:41:51 +0100

    initial setup - blank expo typescript project

--- App.tsx
+++ App.tsx
@@ -1,20 +1,31 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+
+const Tab = createBottomTabNavigator();
+
+function MapScreen() {
+  return <View style={{flex:1,alignItems:'center',justifyContent:'center'}}><Text>Map</Text></View>;
+}
+
+function ScanScreen() {
+  return <View style={{flex:1,alignItems:'center',justifyContent:'center'}}><Text>Scan</Text></View>;
+}
+
+function EncyclopaediaScreen() {
+  return <View style={{flex:1,alignItems:'center',justifyContent:'center'}}><Text>My Plants</Text></View>;
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <NavigationContainer>
       <StatusBar style="auto" />
-    </View>
+      <Tab.Navigator>
+        <Tab.Screen name="Map" component={MapScreen} />
+        <Tab.Screen name="Scan" component={ScanScreen} />
+        <Tab.Screen name="Plants" component={EncyclopaediaScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
